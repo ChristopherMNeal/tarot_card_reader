32b688fdb43c40e30aa01c00b7225f9424c46dd0
Frank-Proulx <frankproulx7@gmail.com>
2022-01-24 11:03:31 -0800

    complete seed file, begin adding views Co-authored-by: Morgan Waites <morgan.waites@gmail.com> Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com> Co-authored-by: Matt Casperson <mc.casperson@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_170722) do
+ActiveRecord::Schema.define(version: 2022_01_24_173353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_01_24_170722) do
     t.string "name"
     t.string "description"
     t.string "attached_image"
+    t.string "reverse_description"
   end
 
 end
