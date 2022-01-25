0a4d7ea1b232ed6664db8029df173cc50f0e9eec
mellowmorgan <morgan.waites@gmail.com>
2022-01-24 17:39:15 -0800

    link up js file add click listeners

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,15 @@
+ $(document).ready(function(){
+  $("#back-card-1").click( function(){
+    $("#back-card-1").hide();
+    $("#card-image-1").show();
+  });
+  $("#back-card-2").click( function(){
+    $("#back-card-2").hide();
+    $("#card-image-2").show();
+  });
+  $("#back-card-3").click( function(){
+    $("#back-card-3").hide();
+    $("#card-image-3").show();
+  });
+});
+  
