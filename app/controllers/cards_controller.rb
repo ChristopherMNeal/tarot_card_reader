fc3a1c5456d704d7ea5c620824a305ac0061aece
Frank-Proulx <frankproulx7@gmail.com>
2022-01-27 10:27:16 -0800

    add reverse page for ten card, final tweaks to css
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,44 +4,44 @@ class CardsController < ApplicationController
     render :index
   end
 
-  def new
-    @card = Card.new
-    render :new
-  end
+  # def new
+  #   @card = Card.new
+  #   render :new
+  # end
 
-  def create
-    @card = Card.new(card_params)
-    if @card.save
-      redirect_to cards_path
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @card = Card.new(card_params)
+  #   if @card.save
+  #     redirect_to cards_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def edit
-    @card = Card.find(params[:id])
-    render :edit
-  end
+  # def edit
+  #   @card = Card.find(params[:id])
+  #   render :edit
+  # end
 
   def show
     @card = Card.find(params[:id])
     render :show
   end
 
-  def update
-    @card = Card.find(params[:id])
-    if @card.update(card_params)
-      redirect_to cards_path
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   @card = Card.find(params[:id])
+  #   if @card.update(card_params)
+  #     redirect_to cards_path
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def destroy
-    @card = Card.find(params[:id])
-    @card.destroy
-    redirect_to cards_path
-  end
+  # def destroy
+  #   @card = Card.find(params[:id])
+  #   @card.destroy
+  #   redirect_to cards_path
+  # end
 
   def three
     @cards = Card.all.shuffle
@@ -52,18 +52,26 @@ class CardsController < ApplicationController
   def ten
     @cards = Card.all.shuffle
     @ten = @cards.slice(0,10)
+    # @ten.map { |card| card.reversed *= rand(1..2) }
+    # @reversed = false
+    render :ten
+  end
+
+  def reverse
+    @cards = Card.all.shuffle
+    @ten = @cards.slice(0,10)
     @ten.map { |card| card.reversed *= rand(1..2) }
     @reversed = false
-    render :ten
+    render :reverse
   end
 
   def random
     @card = Card.all.sample
-    render :random
+    render :show
   end
 
-  private
-    def card_params
-      params.require(:card).permit(:name, :description, :image)
-    end
+  # private
+  #   def card_params
+  #     params.require(:card).permit(:name, :description, :image)
+  #   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'cards/random' => 'cards#random'
   get 'cards/three' => 'cards#three'
   get 'cards/ten' => 'cards#ten'
+  get 'cards/reverse' => 'cards#reverse'
   root to: 'home#index'
-  resources :cards
+  resources :cards, only: [:index, :show]
 end
